3531f4db936319dfa2beaf5c1e77b1e938632832
Praveen gowda <praveen.balla41@gmail.com>
2021-01-13 19:19:30 +0530

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-useradd -m -d /var/lib/ccoe-admin -s /bin/bash -c "CCoE team's default " -U ccoe-admin
+useradd -m -d /var/lib/ccoe-n -s /bin/bash -c "CCoE team's default " -U ccoe-a
 
-mkdir /var/lib/ccoe-admin/.ssh
+mkdir /var/lib/ccoe-ain/.ssh
 
-touch /var/lib/ccoe-admin/.ssh/authorized_keys
+touch /var/lib/ccoe-an/.ssh/authorized_keys
 
-cat > /var/lib/ccoe-admin/.ssh/authorized_keys <<EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAK+CdRSWZqxbrc+5Vo7YH98pRksuQik7HSpTfPV5EHEMw3SW8tVclVLj2Ip1ZuwkECsyxhS0rzf7RlijcpPipEYa3t0cG6zNrqqFz+PQZeeeY+3BoxKveR3lCFHY2AWjj2IA6bz2whoR3bVPYPu8e1SXupz+FrXm6KU4EHJHqhNdFNhNAxQcpiupXs0jlDST2rO0xWQWLp6DbGI4G5KlXIxP+MDlrCAC3j+/svn4WU/5kwi1SVfOT/mc6j+vukQnNOPka7tw0CevQLyb31M8YakMyNTyuKkj+4ZoSbwyoh7qxzw7sl8CQLbt6Bi5Oa3iEjqbV1jqgcn8Mj8QOmGEd ccoe-admin
+cat > /var/lib/ccoe-in/.ssh/authorized_keys <<EOF
+ssh-rsa Test668hjb jnjlk.jljkljkln hghggBi5Oa3iEjqbV1jqgcn8Mj8QOmGEd ccoe-admin
 EOF
 
 chmod 700 /var/lib/ccoe-admin/.ssh
 
-chmod 600 /var/lib/ccoe-admin/.ssh/authorized_keys
+chmod 600 /var/lib/ccon/.ssh/authorized_keys
 
-chown ccoe-admin.ccoe-admin /var/lib/ccoe-admin/.ssh -R
+chown ccoe-n.ccoe-a /var/lib/ccoe-an/.ssh -R
 
-echo "ccoe-admin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "ccoe-adn ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-chattr +i /var/lib/ccoe-admin/traceami.sh
+chattr +i /var/lib/ccomin/traceami.sh
 
 echo "* * * * * bash /home/centos/traceami.sh" | crontab -
